05b2a9c4bc082c51ddb6121c3355156fe72b6f05
shorty0207 <vytguc@gmail.com>
2021-02-16 17:43:49 +0200

    tested Application constructor

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <?php
+require_once 'core/Application.php';
+
+use app\core\Application;
+
 
 $app = new Application();
 
 
-$router = new Router();
 
-$router->get('/', function () {
-    return "this is home page";
-});
+//$router = new Router();
+//
+//$router->get('/', function () {
+//    return "this is home page";
+//});
+//
+//$router->get('/about', function () {
+//    return "this is about page";
+//});
 
-$router->get('/about', function () {
-    return "this is about page";
-});
-
-$app->run();
+//$app->run();
